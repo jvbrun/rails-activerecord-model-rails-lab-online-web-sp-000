613758e15a73f4aca8b5a0e112a68cdbf77ffcc7
Jean-Victor Brun <jeanvbrun@icloud.com>
2021-04-05 14:30:14 +0000

    Automatically backed up by Learn

--- app/views/student/index.html.rb
+++ app/views/student/index.html.rb
@@ -1,7 +1,6 @@
 <h1>Student Records</h1>
 <ol>
 <%= @student.each do |student| %>
-<li><%= Student.first_name %></li>
-<li><%= Student.last_name %></li>
+<li><%= Student.first_name, Student.last_name %></li>
 <% end %>
 </ol>
